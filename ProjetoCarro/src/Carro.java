98340a4a50e807f8778a3d95d6d7818c0c646e4f
Ioly Mayara Mesquita Oliveira <87779906+iolymmoliveira@users.noreply.github.com>
2023-08-01 21:55:57 -0300

    Add files via upload

--- ProjetoCarro/src/Carro.java
+++ ProjetoCarro/src/Carro.java
@@ -1,33 +1,84 @@
 //Definição do tipo de dado "Carro"
 public class Carro {
-	//características
-	String marca;
-	String modelo;
-	int	   ano;
-	String cor;
-	int    km;
-	double preco;
+	// -----  Atributos == características ----- //
+	private String marca;
+	private String modelo;
+	private int	   ano;
+	private String cor;
+	private int    km;
+	private double preco;
+	// ----------------------------------------- //
 	
-	//Método == funcionalidade	
-	void exibirAnuncio () {
+	// ----- Getters and Setters ----- //
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;					//This dos setters sempre se referem ao atributo. 
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public int getKm() {
+		return km;
+	}
+
+	public void setKm(int km) {
+		this.km = km;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}	
+	// ------------------------------ //
+
+	//  ----- Métodos == Funcionalidades  ----- //	
+	public void exibirAnuncio () {
 		System.out.println("Anúncio do Carro:");
-		System.out.println(marca + "-" + modelo + "(" + ano + ")");
-		System.out.println("Cor: " + cor + " Km: " + km + " R$ " + preco);
+		System.out.println(this.marca + "-" + this.modelo + "(" + this.ano + ")");
+		System.out.println("Cor: " + this.cor + " Km: " + this.km + " R$ " + this.preco);
 		System.out.println();
 	}
 	
-	void simularSeguro(int perfil) {
+	public void simularSeguro(int perfil) {
 		if (perfil == 0) {
 			System.out.println("Perfil: Idoso - 1.5%");
-			System.out.println("R$ " + preco * 0.015);
+			System.out.println("R$ " + this.preco * 0.015);
 		}
 		else if (perfil == 1) {
 			System.out.println("Perfil: Chefe de Família - 2.5%");
-			System.out.println("R$ " + preco * 0.025);
+			System.out.println("R$ " + this.preco * 0.025);
 		}
 		else if (perfil == 2) {
 			System.out.println("Perfil: Jovem - 14%");
-			System.out.println("R$ " + preco * 0.14);
+			System.out.println("R$ " + this.preco * 0.14);
 		}
 		else {
 			System.out.println("Não possuimos este Perfil.");
@@ -35,15 +86,16 @@ public class Carro {
 		}
 	}
 	
-	double calcularIPVA(){
+	public double calcularIPVA(){
 		double valorIPVA;
-		if (ano <= 2002) {
+		if (this.ano <= 2002) {
 			valorIPVA = 0;
 		}
 		else {
-			valorIPVA = preco * 0.035;
+			valorIPVA = this.preco * 0.035;
 		}
 		return valorIPVA;
 	}
+//  ----------------------------------------- //	
 	
 }
--- ProjetoCarro/src/Loja.java
+++ ProjetoCarro/src/Loja.java
@@ -7,19 +7,19 @@ public class Loja {
 		c1 = new Carro();
 		c2 = new Carro();
 		
-		c1.marca = "Porcsche";
-		c1.modelo = "Cayenne";
-		c1.ano = 2022;
-		c1.km = 0;
-		c1.cor = "Branco";
-		c1.preco = 2200000;
+		c1.setMarca("Porcsche");
+		c1.setModelo("Cayenne");
+		c1.setAno(2022);
+		c1.setKm(0);
+		c1.setCor("Branco");
+		c1.setPreco(2200000);
 		
-		c2.marca = "Chevrolet";
-		c2.modelo = "Corsa";
-		c2.ano = 1998;
-		c2.km = 250000;
-		c2.cor = "Prata";
-		c2.preco = 8000;
+		c2.setMarca("Chevrolet");
+		c2.setModelo("Corsa");
+		c2.setAno(1998);
+		c2.setKm(250000);
+		c2.setCor("Prata");
+		c2.setPreco(8000);
 		
 		c1.exibirAnuncio();
 		c1.simularSeguro(2);
